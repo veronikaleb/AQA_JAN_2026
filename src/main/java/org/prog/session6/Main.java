ae8dc3a049cdaa39ec30b55ce3b3b3761742f21c
Veronika <maskaarhangela@gmail.com>
2026-02-02 14:36:23 +0200

    homework 6

--- src/main/java/org/prog/session6/Main.java
+++ src/main/java/org/prog/session6/Main.java
@@ -1,23 +1,12 @@
 package org.prog.session6;
 
 public class Main {
-
     public static void main(String[] args) {
-//        int i = Calc.add(1, 10);
-//        String s = Calc.concat("1", "2");
-//
-//        System.out.println(i);
-//        System.out.println(s);
+        Truck myTruck = new Truck();
+        myTruck.setTruckInfo("Volvo", "Maksym", "AA1234DC");
 
-        System.out.println(Calc.countTo(20));
-        System.out.println(Calc.countTo(49));
-        System.out.println(Calc.countTo(100));
-        Calc.countFromTo(30, 60);
 
-//        Truck truck1 = new Truck();
-//        truck1.name = "Vasya";
-//        truck1.delivery("Lviv", "Kyiv");
-//        truck1.delivery("Kyiv", "Odessa");
-//        truck1.delivery("Odessa", "Kyiv");
+        System.out.println(myTruck.getTruckDetails());
+        myTruck.delivery("Kyiv", "Vinnytsia");
     }
 }
--- src/main/java/org/prog/session6/Truck.java
+++ src/main/java/org/prog/session6/Truck.java
@@ -4,17 +4,22 @@ package org.prog.session6;
 //TODO: add method which will return owner name, model and plate number
 //TODO: use String to store and print this in Main
 //TODO: * - make array of trucks
+
 public class Truck {
 
     public String model;
     public String name;
+    public String plateNumber;
 
-    public void setTruckInfo(String m, String n) {
+    public void setTruckInfo(String m, String n, String p) {
         model = m;
         name = n;
+        plateNumber = p;
     }
-
+    public String getTruckDetails() {
+        return "Driver: " + name + ", Car model: " + model + ", Plate Number: " + plateNumber;
+    }
     public void delivery(String from, String to) {
-        System.out.println("Delivering " + from + " to " + to + " by " + name);
+        System.out.println("Delivering from " + from + " to " + to + " by " + name);
     }
 }
