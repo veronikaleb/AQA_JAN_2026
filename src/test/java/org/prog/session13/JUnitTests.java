676e4a56c35f8d3fed97e22caf44867e375347c9
Veronika <maskaarhangela@gmail.com>
2026-02-27 13:13:30 +0200

    homework 13

--- src/test/java/org/prog/session13/JUnitTests.java
+++ src/test/java/org/prog/session13/JUnitTests.java
@@ -2,44 +2,28 @@ package org.prog.session13;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.util.Random;
-
-//TODO: Using class Androind OR Apple:
-//TODO: Add field int modelNumber
-//TODO: Assign model as random 1-5
-//TODO: Write test that confirms model number is 3 or above
 
 public class JUnitTests {
 
     private Random random = new Random();
 
-    @Test
-    public void test1() {
-        int i = random.nextInt(100);
-        Assertions.assertTrue(i >= 70);
-        System.out.println("==== TEST 1 ====");
+    class Android {
+        int modelNumber;
+
+        public Android() {
+            this.modelNumber = random.nextInt(5) + 1;
+        }
     }
 
     @Test
-    public void test2() {
-        int i = random.nextInt(100);
-        Assertions.assertTrue(i >= 70);
-        System.out.println("==== TEST 2 ====");
+    public void testAndroidModel() {
+        Android myPhone = new Android();
+
+        System.out.println("==== Generated Android Model: " + myPhone.modelNumber + " ====");
+
+        Assertions.assertTrue(myPhone.modelNumber >= 3,
+                "Model number must be 3 or above! Current: " + myPhone.modelNumber);
     }
 
-    @Test
-    public void test3() {
-        int i = random.nextInt(100);
-        Assertions.assertTrue(i >= 70);
-        System.out.println("==== TEST 3 ====");
-    }
-
-    public void smth(String s) {
-        System.out.println(s.length());
-    }
-
-    public int randomInt() {
-        return random.nextInt(100);
-    }
 }
